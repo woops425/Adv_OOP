151cf93d755cc22ed5bf086bb473611d4713f339
woops425 <162817809+woops425@users.noreply.github.com>
2025-10-01 23:42:07 +0900

    Delete FakePrinter.java

--- FakePrinter.java
+++
@@ -1,8 +0,0 @@
-package Hw5_1;
-
-public class FakePrinter implements ReceiptPrinter {
-    //***************************
-// 파일명: FakePrinter.java
-// 작성자: 202010629 박상우
-// 작성일: 2025.10.01
-// 내용: Rece
